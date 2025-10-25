1052f9b393a225d249862fdd7a71227f2978572b
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2025-10-25 09:16:51 +0300

    Fixed linters errors

--- src/app/(routes)/products/[id]/page.tsx
+++ src/app/(routes)/products/[id]/page.tsx
@@ -6,11 +6,11 @@ import { Loader } from "@/shared/ui/Loader";
 const ProductDetailsPageClient = lazy(() => import("@/pages-client/ProductDetailsPageClient"));
 
 export async function generateMetadata({ params }: { params: { id: string } }) {
-  return generateProductMetadata((await params).id);
+  return generateProductMetadata(params.id);
 }
 
 export default async function ProductPage({ params }: { params: { id: string } }) {
-  const id = +(await params).id;
+  const id = +params.id;
   const product = await fetchProductServer(id, { cache: "no-store" });
 
   return (
