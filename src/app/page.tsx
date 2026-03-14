02f5c362569f76afc331b57ef96771660a0062e0
Artem Makarchenko <artem.makarchenko.dev@gmail.com>
2026-03-14 12:31:00 +0200

    Add button for checking BE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import { FeaturedProducts } from "@/widgets/home/ui/FeaturedProducts";
 import { Hero } from "@/widgets/home/ui/Hero";
 import { Newsletter } from "@/widgets/home/ui/Newsletter";
 import { ValueProps } from "@/widgets/home/ui/ValueProps";
+import { HealthCheckButton } from "@/features/health-check/ui/HealthCheckButton";
 
 export default function HomePage() {
   return (
     <div className="space-y-20">
       <Hero />
+      <HealthCheckButton />
       <Categories />
       <FeaturedProducts />
       <ValueProps />
